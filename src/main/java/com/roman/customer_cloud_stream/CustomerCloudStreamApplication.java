4fed433ea9a1043e848abbb13e7b99d497b3b4f7
Aplodise <ronmacdonald228@gmail.com>
2024-09-08 17:22:54 +0200

    Initial commit

---
+++ src/main/java/com/roman/customer_cloud_stream/CustomerCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.roman.customer_cloud_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCloudStreamApplication.class, args);
+	}
+
+}
