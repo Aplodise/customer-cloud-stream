4a6680250c181e230fcea1fa12a12ffac3793078
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-09 16:15:30 +0200

    Emitting CustomerEvent instead of Customer, CustomerMapper interface to map Customer to CustomerDTO needed for CustomerEvent

--- src/main/java/com/roman/customer_cloud_stream/service/CustomerServiceImpl.java
+++ src/main/java/com/roman/customer_cloud_stream/service/CustomerServiceImpl.java
@@ -2,11 +2,13 @@ package com.roman.customer_cloud_stream.service;
 
 import com.roman.customer_cloud_stream.domain.Customer;
 import com.roman.customer_cloud_stream.domain.EmailAddress;
+import com.roman.customer_cloud_stream.messaging.event.CustomerDTO;
+import com.roman.customer_cloud_stream.messaging.event.CustomerEvent;
 import com.roman.customer_cloud_stream.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Sinks;
-
+import java.time.Instant;
 import java.util.NoSuchElementException;
 
 
@@ -14,12 +16,14 @@ import java.util.NoSuchElementException;
 @Service
 public class CustomerServiceImpl implements CustomerService{
     private final CustomerRepository customerRepository;
-    private final Sinks.Many<Customer> customerProducer;
+    private final Sinks.Many<CustomerEvent> customerProducer;
 
     @Override
     public Customer createCustomer(final Customer customer) {
         Customer createdCustomer = customerRepository.save(customer);
-        customerProducer.tryEmitNext(createdCustomer);
+        var customerCreatedEvent = new CustomerEvent.CustomerCreated(createdCustomer.getId(),
+                Instant.now(), CustomerMapper.mapToCustomerDTO(createdCustomer));
+        customerProducer.tryEmitNext(customerCreatedEvent);
         return createdCustomer;
     }
 
@@ -28,5 +32,15 @@ public class CustomerServiceImpl implements CustomerService{
         Customer customer = customerRepository.findById(customerId).orElseThrow(
                 () -> new NoSuchElementException(String.format("Couldn't find a customer by id: %s", customerId)));
         customer.changeEmail(emailAddress);
+    }
+
+    interface CustomerMapper {
+        static CustomerDTO mapToCustomerDTO(final Customer customerCreated) {
+            return new CustomerDTO(customerCreated.getFirstName().firstName(),
+                    customerCreated.getLastName().lastName(),
+                    customerCreated.getBirthDate().birthDate(),
+                    customerCreated.getEmailAddress().emailAddress(),
+                    customerCreated.getSsn().ssn());
+        }
     }
 }
