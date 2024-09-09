ce6d2fb41e8206c4d782cba63ae9cd39632e433f
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-09 12:35:06 +0200

    CustomerDTO

---
+++ src/main/java/com/roman/customer_cloud_stream/controller/dto/CustomerDTO.java
@@ -0,0 +1,20 @@
+package com.roman.customer_cloud_stream.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+import java.time.LocalDate;
+
+public record CustomerDTO(@NotBlank(message = "first name cannot be blank")
+                          String firstName,
+                          @NotBlank(message = "last name cannot be blank")
+                          String lastName,
+                          @NotNull(message = "the birthdate cannot be null")
+                          @Past(message = "the birthdate should be in the past")
+                          LocalDate birthDate,
+                          @NotBlank(message = "email cannot be blank")
+                          @Email(message = "please provide a valid email address")
+                          String emailAddress) {
+}
