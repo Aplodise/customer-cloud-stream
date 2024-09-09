937e7d3d4c788b718d6e0dc02ec4f1ac60d19861
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-09 16:14:21 +0200

    Customer to CustomerEvent change

--- src/main/java/com/roman/customer_cloud_stream/messaging/CustomerMessaging.java
+++ src/main/java/com/roman/customer_cloud_stream/messaging/CustomerMessaging.java
@@ -1,6 +1,7 @@
 package com.roman.customer_cloud_stream.messaging;
 
-import com.roman.customer_cloud_stream.domain.*;
+
+import com.roman.customer_cloud_stream.messaging.event.CustomerEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
@@ -11,14 +12,14 @@ import java.util.function.Supplier;
 public class CustomerMessaging {
 
     @Bean
-    public Sinks.Many<Customer> customerProducer(){
+    public Sinks.Many<CustomerEvent> customerProducer(){
         return Sinks.many()
                 .replay()
                 .latest();
     }
 
     @Bean
-    public Supplier<Flux<Customer>> customerSupplier(){
+    public Supplier<Flux<CustomerEvent>> customerSupplier(){
         return () -> customerProducer().asFlux();
     }
 }
