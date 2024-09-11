6b55ac1d269d5c3f528d07750a5188b8e9452ab6
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-11 16:13:09 +0200

    Transactional added

--- src/main/java/com/roman/customer_cloud_stream/service/CustomerServiceImpl.java
+++ src/main/java/com/roman/customer_cloud_stream/service/CustomerServiceImpl.java
@@ -7,6 +7,7 @@ import com.roman.customer_cloud_stream.messaging.event.CustomerEvent;
 import com.roman.customer_cloud_stream.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Sinks;
 import java.time.Instant;
 import java.util.NoSuchElementException;
@@ -18,6 +19,7 @@ public class CustomerServiceImpl implements CustomerService{
     private final CustomerRepository customerRepository;
     private final Sinks.Many<CustomerEvent> customerProducer;
 
+    @Transactional
     @Override
     public Customer createCustomer(final Customer customer) {
         Customer createdCustomer = customerRepository.save(customer);
@@ -27,11 +29,13 @@ public class CustomerServiceImpl implements CustomerService{
         return createdCustomer;
     }
 
+    @Transactional
     @Override
     public void changeEmail(final Long customerId, final EmailAddress emailAddress) {
         Customer customer = customerRepository.findById(customerId).orElseThrow(
                 () -> new NoSuchElementException(String.format("Couldn't find a customer by id: %s", customerId)));
         customer.changeEmail(emailAddress);
+        customerProducer.tryEmitNext(new CustomerEvent.EmailChanged(customer.getId(), Instant.now(), CustomerMapper.mapToCustomerDTO(customer)));
     }
 
     interface CustomerMapper {
